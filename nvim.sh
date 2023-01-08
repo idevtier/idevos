51dbf8b57f23543f2fec456ddc294abf6e957fb0
idevtier <idevtier@gmail.com>
2023-01-08 17:43:29 +0300

    feat(nvim): add base setup

--- nvim.sh
+++ nvim.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt update -y
+sudo apt-get install -y neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+if [ -d "$HOME/.config/nvim" ]; then
+    mkdir -p "$HOME/old_files"
+    mv "$HOME/.config/nvim" "$HOME/old_files"
+fi
+git clone "https://github.com/idevtier/nvim" "$HOME/.config/nvim"
+
