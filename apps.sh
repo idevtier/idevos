a94335b832ceff136688bc0d623c694029ad78b8
idevtier <idevtier@gmail.com>
2023-01-08 15:28:55 +0300

    fix(apps): add fish installation

--- apps.sh
+++ apps.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+sudo apt-add-repository ppa:fish-shell/release-3 
 sudo apt-get update -y
-sudo apt-get install -y git
+sudo apt-get upgrade -y
+sudo apt-get install -y git fish
