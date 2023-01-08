ccaa1c32b837bab860760cf8baa78f024a00d055
idevtier <idevtier@gmail.com>
2023-01-08 17:00:59 +0300

    fix(apps): better apps install

--- apps.sh
+++ apps.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-add-repository ppa:fish-shell/release-3 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y git fish
+sudo apt-get install -y git fish fzf lazygit pyenv
